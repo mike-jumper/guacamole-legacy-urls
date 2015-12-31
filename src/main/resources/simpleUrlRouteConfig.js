49629cda294449b1c8d1a0b1b8d9a4c3213a85b1
Michael Jumper <mike.jumper@guac-dev.org>
2015-12-31 02:24:03 -0800

    Automatically redirect requests for "/connection/[CONNECTION IDENTIFIER]" to "/client/[CLIENT IDENTIFIER]".

--- src/main/resources/simpleUrlRouteConfig.js
+++ src/main/resources/simpleUrlRouteConfig.js
@@ -21,41 +21,77 @@
  */
 
 /**
- * Config block augmenting Guacamole's existing URL routing.
+ * Config block augmenting Guacamole's existing URL routing, automatically
+ * redirecting requests from "/connection/[CONNECTION IDENTIFIER]" to
+ * "/client/[CLIENT IDENTIFIER]".
  */
 angular.module('simpleUrl').config(['$routeProvider',
         function simpleUrlRouteConfig($routeProvider) {
 
     /**
-     * Attempts to re-authenticate with the Guacamole server, sending any
-     * query parameters in the URL, along with the current auth token, and
-     * updating locally stored token if necessary.
+     * Re-authenticates with the Guacamole server and, if successful, redirects
+     * to from "/connection/[CONNECTION IDENTIFIER]" to the proper
+     * "/client/[CLIENT IDENTIFIER]" URL. The client identifier is derived
+     * assuming that the destination is a connection (not a group) and the
+     * data source is the default data source (the data source which
+     * authenticated the user).
      *
      * @param {Service} $injector
      *     The Angular $injector service.
      *
      * @returns {Promise}
-     *     A promise which resolves successfully only after an attempt to
-     *     re-authenticate has been made. If the authentication attempt fails,
-     *     the promise will be rejected.
+     *     A promise which is rejected once the correct URL has been determined
+     *     and the user has been redirected, or once authentication has failed.
      */
-    var updateCurrentToken = ['$injector', function updateCurrentToken($injector) {
+    var redirectToClient = ['$injector', function redirectToClient($injector) {
+
+        // Required types
+        var ClientIdentifier = $injector.get('ClientIdentifier');
 
         // Required services
         var $location             = $injector.get('$location');
+        var $q                    = $injector.get('$q');
+        var $route                = $injector.get('$route');
         var authenticationService = $injector.get('authenticationService');
 
+        // Promise for routing attempt
+        var route = $q.defer();
+
         // Re-authenticate including any parameters in URL
-        return authenticationService.updateCurrentToken($location.search());
+        authenticationService.updateCurrentToken($location.search())
+        .then(function reauthenticationSucceeded() {
+
+            // Get connection identifier from parameters
+            var identifier = $route.current.params.id;
+
+            // Derive client identifier directly from connection identifier
+            var clientIdentifier = ClientIdentifier.toString({
+                dataSource : authenticationService.getDataSource(),
+                type       : ClientIdentifier.Types.CONNECTION,
+                id         : identifier
+            });
+
+            // Redirect to proper client URL
+            $location.url('/client/' + encodeURIComponent(clientIdentifier));
+            route.reject();
+
+        })
+
+        // Simply ignore if authentication fails
+        ['catch'](function reauthenticationFailed() {
+            route.reject();
+        });
+
+        // Return promise that will be rejected once either (1) the correct
+        // client URL has been determined or (2) the correct client URL cannot
+        // be determined because authentication has failed
+        return route.promise;
 
     }];
 
-    // Configure each possible route
-    $routeProvider.when('/client2/:id/:params?', {
-        bodyClassName : 'client',
-        templateUrl   : 'app/client/templates/client.html',
-        controller    : 'clientController',
-        resolve       : { updateCurrentToken: updateCurrentToken }
+    // Redirect /connection/IDENTIFIER to /client/...
+    $routeProvider.when('/connection/:id/:params?', {
+        resolve       : { redirectToClient : redirectToClient }
     });
 
 }]);
